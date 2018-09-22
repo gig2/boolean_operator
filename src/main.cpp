2b43bc76edb845bb9677fe50402132b7be449709
Thibault Payet <monwarez@gmail.com>
2018-09-22 11:33:50 +0200

    add ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+
+#include "gui/simpleWindow.h"
+
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+#include <QApplication>
+
 #include <iostream>
 
-int main(int argc, char** argv)
+
+
+using MyMesh = OpenMesh::PolyMesh_ArrayKernelT<>;
+
+int main( int argc, char** argv )
 {
-    return 0;
+    QApplication app( argc, argv );
+
+    SimpleWindow window;
+
+    window.show();
+
+    return app.exec();
 }
