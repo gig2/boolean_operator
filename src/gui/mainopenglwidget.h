6589686305422f28d5c0d7e00cbd63e9d74fc55d
Thibault Payet <monwarez@gmail.com>
2018-10-16 16:00:27 +0200

    start adding meshnode

--- src/gui/mainopenglwidget.h
+++ src/gui/mainopenglwidget.h
@@ -1,12 +1,17 @@
 #pragma once
 
+#if 0
 #include "engine/Engine-main.h"
 #include "engine/Mesh.h"
 #include "engine/MeshManager.h"
 #include "tools/Loader.h"
-/* #include <QOpenGLFunctions> */
+#endif
+
+#include <GL/glew.h>
+
 #include <QOpenGLWidget>
 #include <QWidget>
+
 
 
 #include <chrono>
@@ -26,12 +31,163 @@ public:
 };
 
 
-class MainOpenGLWidget : public QOpenGLWidget //, protected QOpenGLFunctions
+// Mesh file
+
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/Attributes.hh>
+#include <OpenMesh/Core/Mesh/Handles.hh>
+#include <OpenMesh/Core/Mesh/Traits.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+
+
+struct MeshTraits : public OpenMesh::DefaultTraits
+{
+    // use floating point
+    typedef OpenMesh::Vec3f Point;
+
+    // use vertex normals and vertex colors
+    VertexAttributes( OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color );
+
+
+    // use face normals
+    FaceAttributes( OpenMesh::Attributes::Normal );
+};
+
+class MeshNode
+{
+public:
+    MeshNode( std::string const &filename )
+    {
+        //
+        if ( !OpenMesh::IO::read_mesh( mesh_, filename ) )
+        {
+            throw std::runtime_error( std::string( "Read error for file: " ) + filename );
+        }
+    }
+
+    void updateVertexBuffer( int const positionLocation, int const colorLocation )
+    {
+        //
+        numIndexes_ = mesh_.n_faces() * verticesNumberPerFace_;
+
+        auto *vertexPointBuffer = mesh_.points();
+
+        std::vector<unsigned int> indexes;
+        indexes.reserve( numIndexes_ );
+
+
+
+        std::for_each(
+            mesh_.faces_begin(), mesh_.faces_end(), [this, &indexes]( auto const &faces ) {
+                std::transform( mesh_.cfv_begin( faces ), mesh_.cfv_end( faces ),
+                                std::back_inserter( indexes ),
+                                []( auto const &faceVertex ) { return faceVertex.idx(); } );
+            } );
+
+
+        // let's put a color
+        std::vector<GLfloat> colors;
+        colors.resize( mesh_.n_vertices() * numberColors_ );
+        for ( auto &cols : colors )
+        {
+            cols = 0.5;
+        }
+
+
+        regenerateBuffers_();
+
+        glBindVertexArray( vao_ );
+
+        glBindBuffer( GL_ARRAY_BUFFER, vertexBuffer_ );
+        glBufferData( GL_ARRAY_BUFFER,
+                      sizeof( decltype( *vertexPointBuffer ) ) * mesh_.n_vertices(),
+                      vertexPointBuffer, GL_STATIC_DRAW );
+
+        glVertexAttribPointer( positionLocation, 3, GL_FLOAT, GL_FALSE,
+                               sizeof( decltype( *vertexPointBuffer ) ), 0 );
+        glEnableVertexAttribArray( positionLocation );
+
+        glBindBuffer( GL_ARRAY_BUFFER, colorBuffer_ );
+        glBufferData( GL_ARRAY_BUFFER, sizeof( GLfloat ) * colors.size(), colors.data(),
+                      GL_STATIC_DRAW );
+
+        glVertexAttribPointer( colorLocation, 3, GL_FLOAT, GL_FALSE, sizeof( GLfloat ) * 3, 0 );
+        glEnableVertexAttribArray( colorLocation );
+
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, indexBuffer_ );
+        glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( unsigned int ) * numIndexes_, indexes.data(),
+                      GL_STATIC_DRAW );
+
+        glBindVertexArray( 0 );
+    }
+
+    void draw()
+    {
+        //
+    }
+
+    ~MeshNode() { cleanBuffers_(); }
+
+private:
+    OpenMesh::TriMesh_ArrayKernelT<MeshTraits> mesh_;
+
+    GLuint vao_{0};
+    GLuint indexBuffer_{0};
+    GLuint vertexBuffer_{0};
+    GLuint colorBuffer_{0};
+
+    std::size_t numIndexes_{0};
+
+    const unsigned int verticesNumberPerFace_{3};
+    const unsigned int numberColors_{3};
+
+    void cleanBuffers_()
+    {
+        if ( glIsBuffer( vao_ ) )
+        {
+            glDeleteBuffers( 1, &vao_ );
+        }
+        if ( glIsBuffer( indexBuffer_ ) )
+        {
+            glDeleteBuffers( 1, &indexBuffer_ );
+        }
+        if ( glIsBuffer( vertexBuffer_ ) )
+        {
+            glDeleteBuffers( 1, &vertexBuffer_ );
+        }
+        if ( glIsBuffer( colorBuffer_ ) )
+        {
+            glDeleteBuffers( 1, &colorBuffer_ );
+        }
+    }
+
+    void regenerateBuffers_()
+    {
+        cleanBuffers_();
+
+        glGenBuffers( 1, &vertexBuffer_ );
+        glGenBuffers( 1, &indexBuffer_ );
+        glGenBuffers( 1, &colorBuffer_ );
+
+        glGenVertexArrays( 1, &vao_ );
+    }
+};
+
+
+// end mesh file
+
+class MainOpenGLWidget : public QOpenGLWidget
 {
     Q_OBJECT
 public:
     explicit MainOpenGLWidget( QWidget *parent )
         : QOpenGLWidget( parent )
+        , bunny_{"bunnyLowPoly.obj"}
 
     {
         int major = 3;
@@ -59,6 +215,7 @@ signals:
 public slots:
     void updateAmbient( int value )
     {
+#if 0
         float ambientIntensity = value / 99.;
 
         for ( auto &pl : pointLight_ )
@@ -68,9 +225,12 @@ public slots:
         engine_.AttachLight( pointLight_ );
         ambientValueChanged( static_cast<double>( ambientIntensity ) );
         update();
+
+#endif
     }
     void updateDiffuse( int value )
     {
+#if 0
         float diffuseIntensity = value / 99.;
         for ( auto &pl : pointLight_ )
         {
@@ -79,10 +239,12 @@ public slots:
         engine_.AttachLight( pointLight_ );
         diffuseValueChanged( static_cast<double>( diffuseIntensity ) );
         update();
+#endif
     }
 
     void updateLinear( int value )
     {
+#if 0
         float start = 0.00001;
         float end   = 1.;
 
@@ -97,10 +259,12 @@ public slots:
         engine_.AttachLight( pointLight_ );
         linearValueChanged( static_cast<double>( linear ) );
         update();
+#endif
     }
 
     void updateExp( int value )
     {
+#if 0
         float start = 0.;
         float end   = 0.05;
         float t     = value / 99.;
@@ -113,6 +277,7 @@ public slots:
         engine_.AttachLight( pointLight_ );
         expValueChanged( static_cast<double>( exponential ) );
         update();
+#endif
     }
 
 protected:
@@ -128,8 +293,16 @@ protected:
         }
 
 
-        S3DE::EngineShader engineShader{"shader/color.vs", "shader/color.fs",
-                                        "shader/color.gs", "", ""};
+        // get them from shader
+        int const positionLocation{0};
+        int const colorLocation{1};
+
+        bunny_.updateVertexBuffer( positionLocation, colorLocation );
+
+#if 0
+
+        S3DE::EngineShader engineShader{"shader/color.vs", "shader/color.fs", "shader/color.gs", "",
+                                        ""};
 
         S3DE::EngineWindow engineWindow;
         engineWindow.shader = engineShader;
@@ -207,8 +380,8 @@ protected:
         engine_.AttachLight( spotLight_ );
 
 
+#endif
 
-        // initializeOpenGLFunctions();
         //
         glClearColor( .0f, 0.f, 0.f, .0f );
 
@@ -218,24 +391,29 @@ protected:
     void resizeGL( int width, int height ) override
     {
         //
+#if 0
         float near = 0.01;
         float far  = 100;
         engine_.SetCameraSettings( 70.0, static_cast<float>( width ) / height, near, far );
+#endif
     }
 
     void paintGL() override
     {
         //
-        // glClear( GL_COLOR_BUFFER_BIT );
+        glClear( GL_COLOR_BUFFER_BIT );
 
         currentTime_ = std::chrono::high_resolution_clock::now();
+#if 0
         auto elapsed = currentTime_ - previousTime_;
         engine_.Draw( elapsed );
+#endif
         previousTime_ = currentTime_;
     }
 
 
 private:
+#if 0
     S3DE::CEngine<QtGLWindow> engine_;
     // std::vector<S3DE::MeshData> pmeshData_;
     std::vector<S3DE::PointLight> pointLight_;
@@ -243,8 +421,11 @@ private:
 
     // S3DE::Camera camera_;
 
+#endif
     std::chrono::high_resolution_clock::time_point previousTime_;
     std::chrono::high_resolution_clock::time_point currentTime_;
+
+    MeshNode bunny_;
 
 
 
