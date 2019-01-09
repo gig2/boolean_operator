cc35de03d86fe8bc6a7bccc91c20f9ff4eab5f76
Thibault Payet <monwarez@gmail.com>
2019-01-09 15:41:59 +0100

    start working on octree

---
+++ tests/test-octree/test_octree.cpp
@@ -0,0 +1,20 @@
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include <OpenMesh/Core/Geometry/Vector11T.hh>
+#include <OpenMesh/Core/Mesh/Traits.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
+
+
+using testing::Eq;
+using testing::FloatEq;
+
+using MeshT = OpenMesh::TriMesh_ArrayKernelT<OpenMesh::DefaultTraits>;
+
+
+TEST( bar, doesFoo )
+{
+    //
+}
