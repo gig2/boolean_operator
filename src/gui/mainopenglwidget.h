a863b9683e1683073d448fcb4d21850ca855d3a6
Thibault Payet <monwarez@gmail.com>
2019-01-10 23:07:35 +0100

    use more nice colors

--- src/gui/mainopenglwidget.h
+++ src/gui/mainopenglwidget.h
@@ -24,11 +24,30 @@
 template <typename MeshType>
 void resetColors( MeshType &mesh )
 {
+    using Color = typename MeshType::Color;
+
+    int nVertices = std::distance( mesh.vertices_begin(), mesh.vertices_end() );
+
+    int modulo = std::min( 20, nVertices / 2 + 1 );
+
+    int increment = 0;
+
     // set color for mesh
-    std::for_each( mesh.vertices_begin(), mesh.vertices_end(), [&mesh]( auto &vertex ) {
-        typename Mesh::MeshT::Color color{0.25, 0.5, 0.5};
-        mesh.set_color( vertex, color );
-    } );
+    std::for_each( mesh.vertices_begin(), mesh.vertices_end(),
+                   [&mesh, &modulo, &increment]( auto &vertex ) {
+                       Color color1{0.25, 0.5, 0.5};
+                       Color color2{0.5, 1., 1.};
+
+                       increment %= modulo;
+
+                       float t = increment / static_cast<float>( modulo );
+
+                       Color color = ( 1.f - t ) * color1 + t * color2;
+
+                       mesh.set_color( vertex, color );
+
+                       ++increment;
+                   } );
 }
 
 class MainOpenGLWidget : public QOpenGLWidget
